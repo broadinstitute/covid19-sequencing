5665b36f9215b2d92b102e5d999e2b051c57f439
clu@asymmetrik.com <clu@asymmetrik.com>
2021-05-17 16:24:40 -0400

    Updated with initial change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'broad-covid-dashboard';
+    title = 'broad-covid-dashboard';
+    config:zingchart.graphset = {
+      type: 'line',
+      series: [
+        { values: [3,49,12,23,30, 44] }
+      ]
+    };
+
+    scalingChartWidth = Math.floor(window.innerWidth * 0.66);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ZingchartAngularModule } from 'zingchart-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    ZingchartAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
